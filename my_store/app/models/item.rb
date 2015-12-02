dc354c3b665d44a6a3078db615ffcb90abcd234e
WizardUA <boychuk-o@yandex.ua>
2015-12-02 10:32:53 +0200

    add UI

--- my_store/app/models/item.rb
+++ my_store/app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ActiveRecord::Base
 
-  has_attached_file :item_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :item_image, styles: { medium: "300x300>", thumb: "192x200>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :item_image, content_type: /\Aimage\/.*\Z/
 
   validates :price, numericality: { greater_than: 0, allow_nil: true }
